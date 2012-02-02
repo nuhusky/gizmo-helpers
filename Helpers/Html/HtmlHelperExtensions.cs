0a9f6c23efb94f3cabda2629dd6764445a8b0778
Peter Paperno <peter.paperno@monster.com>
2012-02-02 08:56:50 -0500

    Changed method name for brevity. Fixed build error.

--- Helpers/Html/HtmlHelperExtensions.cs
+++ Helpers/Html/HtmlHelperExtensions.cs
@@ -9,13 +9,13 @@ namespace Gizmo.Helpers.Html
     public static class HtmlHelperExtensions
     {
         /// <summary>
-        /// Builds a SelectListItem list from an enumeration. If enumeration valies is decorated with EnumValueDisplayAttribute then SelectListItem.Text is overridden
+        /// Builds a SelectListItem list from an enumeration. If enumeration value is decorated with EnumValueDisplayAttribute then SelectListItem.Text is overridden
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="htmlHelper"></param>
         /// <param name="enum"></param>
         /// <returns></returns>
-        public static IEnumerable<SelectListItem> DropdownOptionsFromEnum<T>(this HtmlHelper htmlHelper, T @enum) where T : struct, IConvertible
+        public static IEnumerable<SelectListItem> SelectOptionsFromEnum<T>(this HtmlHelper htmlHelper, T @enum) where T : struct, IConvertible
          {
             Type typeOfEnum = typeof (T);
              if (!typeOfEnum.IsEnum)
@@ -28,22 +28,14 @@ namespace Gizmo.Helpers.Html
             FieldInfo[] fields=typeOfEnum.GetFields(BindingFlags.Public | BindingFlags.Static);
             foreach (FieldInfo info in fields)
             {
-                int value = (int) Enum.Parse(typeOfEnum, info.Name);
-
-                EnumValueDisplay attr = info.GetCustomAttributes(typeof (EnumValueDisplay),false).FirstOrDefault() as EnumValueDisplay;
-                if (attr == null)
-                {
-                    desc = info.Name;
-                }
-                else
-                {
-                    desc = attr.Name;
-                }
+                
+                EnumValueDisplayAttribute attr = info.GetCustomAttributes(typeof(EnumValueDisplayAttribute), false).FirstOrDefault() as EnumValueDisplayAttribute;
+                desc = attr == null ? info.Name : attr.Name;
 
                 items.Add(new SelectListItem()
                               {
                                   Text = desc,
-                                  Value =value.ToString()
+                                  Value = Enum.Parse(typeOfEnum, info.Name).ToString()
                               });
 
             }
