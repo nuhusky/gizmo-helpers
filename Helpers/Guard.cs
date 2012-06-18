d83bca239255578268a7b08bddc74de0a0481357
Peter Paperno <peter.paperno@monster.com>
2012-06-18 10:26:32 -0400

    adding guard utility class

---
+++ Helpers/Guard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Gizmo.Helpers
+{
+    static class Guard
+    {
+        
+        public static void NotNullOrEmpty<T>(T valToCheck, string paramName)
+        {
+            Type derivedType = typeof (T);
+            TypeCode derivedTypeCode = Type.GetTypeCode(derivedType);
+            if (derivedTypeCode==TypeCode.String)
+            {
+                if (String.IsNullOrEmpty(valToCheck as String))
+                {
+                    throw new ArgumentNullException(paramName);
+                }
+            } else if (derivedTypeCode==TypeCode.Object)
+            {
+                if (valToCheck as Object == null)
+                {
+                    throw new ArgumentNullException(paramName);
+                }
+            }
+
+           
+        }
+    }
+}
